f7ea0463703b32e8c2bc9b397618bc550a149d9c
rahul baba <55971924+rahulbaba-07@users.noreply.github.com>
2019-10-02 07:32:53 +0530

    Update and rename ASS-4211.cpp to All_ASS-4211.cpp

--- All_ASS-4211.cpp
+++ All_ASS-4211.cpp
@@ -93,3 +93,90 @@ class student
 1st_list_2nd programme..
 
 Fw: #include<iostream> using namespace std; class batsman { 	int bcode; 	string bname; 	int innings,notout,runs; 	float batavg; 	void calcavg() 	{     	batavg=runs/(innings-notout); 	} public : 	void getdata(); 	void displaydata(); }; void batsman::getdata() { 	cout<<"Enter batsman code"; 	cin>>bcode; 	cout<<"Enter batsman name"; 	cin>>bname; cout<<"enter innings,notout and runs"; 	cin>>innings>>notout>>runs; calcavg(); } void batsman::displaydata() { 	cout<<"batsman code"<<bcode<<"\nbatsman name"<<bname<
+
+PROGRAMM NO 7
+#include<iostream>
+using namespace std;
+class Rectangle
+{
+private:
+float length;
+float width;
+public:
+void setlength(float);
+void setwidth(float);
+float perimeter();
+float area();
+void show();
+int sameArea(Rectangle);
+};
+
+
+void Rectangle::setlength(float len)
+{
+length = L;
+}
+
+void Rectangle::setwidth(float wid)
+{
+width = W;
+}
+
+float Rectangle::perimeter()
+{
+return (2 * length + 2 * width);
+}
+
+float Rectangle::area()
+{
+return length * width;
+}
+
+void Rectangle::show()
+{
+cout << "Length: " << length << " Width: " << width;
+}
+
+int Rectangle::sameArea(Rectangle other)
+{
+float areaf = length * width;
+float areas = other.length * other.width;
+if (areaf == areas)
+return 1;
+return 0;
+}
+
+int main()
+{
+Rectangle first;
+Rectangle second;
+first.setlength(5);
+first.setwidth(2.5);
+second.setlength(5);
+second.setwidth(18.9);
+cout << "First rectangle: ";
+first.show();
+cout << endl << "Area: " << first.area() << "Perimeter: " << first.perimeter() << endl << endl;
+cout << "Second rectangle: ";
+second.show();
+cout << endl << "Area: " << second.area() << "Perimeter: " << second.perimeter() << endl << endl;
+if (first.sameArea(second))
+cout << "Rectangles have the same area\n";
+else
+cout << "Rectangles do not have the same area\n";
+first.setlength(15);
+first.setwidth(6.3);
+cout << "First rectangle: ";
+first.show();
+cout << endl << "Area: " << first.area() << "Perimeter: "<< first.perimeter() << endl << endl;
+cout << "Second rectangle: ";
+second.show();
+cout << endl << "Area: " << second.area() << "Perimeter: "<< second.perimeter() << endl << endl;
+if (first.sameArea(second))
+cout << "Rectangles have the same area\n";
+else
+cout << "Rectangles do not have the same area\n";
+
+return 0;
+}
+
